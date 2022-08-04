763acd5fbb4a9bcd8000d154f5e19d9798b5a2ce
Torian Austin <109676477+Pixelisation@users.noreply.github.com>
2022-08-04 12:42:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//Your Code here
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+if(secondAnswer === 'follow'){
+    let thirdAnswer = window.prompt("You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.")
+}
+if(thirdAnswer === 'stay'){
+    let fourthAnswer = window.prompt('You live happily amongst the cats for the rest of your days.')
+}
+else if(thirdAnswer === 'spread the word'){
+    let fourthAnswer = window.prompt('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
+}
+}
+
+else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+if(secondAnswer === "past the dragon"){
+    let thirdAnswer = window.prompt("The dragon wakes up and sits upright. You only have a moment to respond, to stay or run")
+}
+else if(secondAnswer === "away from the dragon"){
+    let thirdAnswer = window.prompt("After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?")
+}
+}
